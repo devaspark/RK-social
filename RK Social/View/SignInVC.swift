1ca5fcf635dc1eaec23b1cd6a35285af332e65e9
rkung <rex.h.kung@gmail.com>
2018-01-05 23:37:09 -0700

    completed setting up Firebase integration

---
+++ RK Social/View/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RK Social
+//
+//  Created by Rex Kung on 1/5/18.
+//  Copyright © 2018 Rex Kung. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
