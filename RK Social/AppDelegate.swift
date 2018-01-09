105171f4754b0b757dccb15044511b0cdb7941fe
rkung <rex.h.kung@gmail.com>
2018-01-09 00:17:55 -0700

    Sign in to FB and Firebase complete

--- RK Social/AppDelegate.swift
+++ RK Social/AppDelegate.swift
@@ -9,6 +9,9 @@
 import UIKit
 import Firebase
 
+import FacebookCore
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -18,6 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
 
@@ -43,6 +48,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    
 }
 
--- RK Social/View/SignInVC.swift
+++ RK Social/View/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,37 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = LoginManager()
 
+        facebookLogin.logIn(readPermissions: [.email], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("Logged In")
+                let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.authenticationToken)
+                self.firebaseAuth(credential)
+            }
+        
+        
+        }
+    
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("REX: Unable to authenticate with firebase = \(String(describing: error))")
+            } else {
+                print("Rex: authenticated to firebase")
+            }
+            
+        })
+        
+    }
 }
 
