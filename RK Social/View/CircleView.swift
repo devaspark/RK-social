ae0d9bf39dfe8b5804e554056a93a169c11864dc
rkung <rex.h.kung@gmail.com>
2018-01-11 23:02:14 -0700

    Completed Feed UI

--- RK Social/View/CircleView.swift
+++ RK Social/View/CircleView.swift
@@ -1,23 +1,30 @@
 //
-//  CustomView.swift
+//  CircleView.swift
 //  RK Social
 //
-//  Created by Rex Kung on 1/7/18.
+//  Created by Rex Kung on 1/11/18.
 //  Copyright © 2018 Rex Kung. All rights reserved.
 //
 
 import UIKit
 
-class CustomView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.opacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
     }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+        
+    }
+    
+        
+    
 
 }
