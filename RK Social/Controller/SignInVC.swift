997aec090cc6cc2b77201ff1d1e8d43e73e35864
rkung <rex.h.kung@gmail.com>
2018-01-12 16:59:36 -0700

    Complete creating Firebase DB Users

--- RK Social/Controller/SignInVC.swift
+++ RK Social/Controller/SignInVC.swift
@@ -62,7 +62,8 @@ class SignInVC: UIViewController {
             } else {
                 print("Rex: authenticated to firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
             
@@ -76,7 +77,8 @@ class SignInVC: UIViewController {
                     if error == nil {
                         print("Rex: Email user authenticated with Firebase")
                         if let user = user {
-                            self.completeSignIn(id: user.uid)
+                            let userData = ["provider": user.providerID]
+                            self.completeSignIn(id: user.uid, userData: userData)
                         }
                     } else {
                         Auth.auth().createUser(withEmail: email, password: pwd, completion: {(user, error) in
@@ -85,7 +87,8 @@ class SignInVC: UIViewController {
                             } else {
                                 print("Rex: Successfully authenticated with Firebase")
                                 if let user = user {
-                                    self.completeSignIn(id: user.uid)
+                                    let userData = ["provider": user.providerID]
+                                    self.completeSignIn(id: user.uid, userData: userData)
                                 }
                             }
                         })
@@ -95,8 +98,8 @@ class SignInVC: UIViewController {
         
     }
     
-    func completeSignIn(id: String) {
-
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("Rex: Data saved to keychain \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
