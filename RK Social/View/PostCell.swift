cff4cf58d2a1499205172fddad0be0030e6dadf2
rkung <rex.h.kung@gmail.com>
2018-01-14 22:11:31 -0700

    Complete Firebase storage download

--- RK Social/View/PostCell.swift
+++ RK Social/View/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
 
@@ -23,11 +24,31 @@ class PostCell: UITableViewCell {
         // Initialization code
     }
 
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage? = nil) {
         self.post = post
         self.caption.text = post.caption
         self.likesLbl.text = "\(post.likes)"
         
+        if img != nil {
+            self.postImg.image = img
+        } else {
+            let ref = Storage.storage().reference(forURL: post.imageUrl)
+            ref.getData(maxSize: 2 * 1024 * 1024, completion: { (data, error) in
+                if error != nil {
+                    // there is an error
+                    print("Rex: Unable to download image from Firebase Storage")
+                } else {
+                    print("Rex: Image downloaded from Firebase storage")
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.postImg.image = img
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                        }
+                    }
+                }
+            })
+            
+        }
     }
     
 
